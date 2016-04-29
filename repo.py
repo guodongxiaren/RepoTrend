b690938b3938e8ef58fd4bfa7c76e52d2bf71c82
guodongxiaren <879231132@qq.com>
2016-04-29 20:12:56 +0800

    改成面向对象的风格

--- repo.py
+++ repo.py
@@ -3,41 +3,42 @@ import urllib2
 import json
 from datetime import date 
 
+class RepoTrend:
+    def __init__(self):
+        self.url = "https://api.github.com/repos/guodongxiaren/README"
 
-def registerUrl():
-    try:
-        url ="https://api.github.com/repos/guodongxiaren/README"
-        data = urllib2.urlopen(url).read()
-        return data
-    except Exception,e:
-        print e
-		
-def saveJsonFile(fileName, fileData):
-	file = open(fileName,"w")
-	jsonStr = json.dumps(fileData)
-	file.write(jsonStr)
-	file.close()
+    def getRemoteData(self):
+        try:
+            self.jsonData = urllib2.urlopen(self.url).read()
+        except Exception,e:
+            print e
+            
+    def saveJsonFile(self, fileName):
+        file = open(fileName,"w")
+        jsonStr = json.dumps(self.trend)
+        file.write(jsonStr)
+        file.close()
 
-def parseJsonFile(fileName):
-	f = file(fileName)
-	s = json.load(f)
-	return s
+    def parseJsonFile(self, fileName):
+        f = file(fileName)
+        self.trend = json.load(f)
 
-def addTodayData(trend, jsonData):
-	value = json.loads(jsonData)
+    def addTodayData(self):
+        value = json.loads(self.jsonData)
 
-	today = date.today().isoformat()
-	fork  = value['forks_count']
-	star  = value['stargazers_count']
+        today = date.today().isoformat()
+        fork  = value['forks_count']
+        star  = value['stargazers_count']
 
-	trend["date"].append(today);
-	trend["fork"].append(fork);
-	trend["star"].append(star);
-	
+        self.trend["date"].append(today);
+        self.trend["fork"].append(fork);
+        self.trend["star"].append(star);
+        
 if __name__ == "__main__":
-	trend = parseJsonFile("trend.json")
-	data = registerUrl()
-	addTodayData(trend, data)
-	saveJsonFile("trend.json", trend)
-	
-	
+    repoTrend = RepoTrend()
+    repoTrend.parseJsonFile("trend.json")
+    repoTrend.getRemoteData()
+    repoTrend.addTodayData()
+    repoTrend.saveJsonFile("trend.json")
+    
+    
